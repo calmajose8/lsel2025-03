a8168d1c430d339058add5cbf882011edbc0a5c1
calmajose8 <calmajose8@gmail.com>
2025-05-26 19:20:00 +0200

    tarea 3a hecha

---
+++ snake-projects/projects/snake/output/stm32_snake_display.d
@@ -0,0 +1,5 @@
+output/stm32_snake_display.o: \
+ port/stm32f411e_acc_lcd/src/stm32_snake_display.c \
+ common/include/snake_display.h common/include/snake.h
+common/include/snake_display.h:
+common/include/snake.h:
---
+++ snake-projects/projects/snake/output/stm32_snake_time.d
@@ -0,0 +1,4 @@
+output/stm32_snake_time.o: port/stm32f411e_acc_lcd/src/stm32_snake_time.c \
+ common/include/snake_time.h common/include/snake.h
+common/include/snake_time.h:
+common/include/snake.h:
