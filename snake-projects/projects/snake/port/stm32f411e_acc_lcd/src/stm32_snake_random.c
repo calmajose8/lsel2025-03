b42dae5c10082b8fd551972758d0f7ae21a117f6
calmajose8 <calmajose8@gmail.com>
2025-05-26 19:38:25 +0200

    3b hecha serio

--- snake-projects/projects/snake/port/stm32f411e_acc_lcd/src/stm32_snake_random.c
+++ snake-projects/projects/snake/port/stm32f411e_acc_lcd/src/stm32_snake_random.c
@@ -1,11 +1,16 @@
 #include "snake_random.h"
+#include "stm32_init.h"
+#include <stdlib.h>
 
 
 int snake_random_init(snake_game_t* p_game){
-    return 1;
+    return stm32_init();
 }
 int snake_random_get(int limit){
-    return 1;
+    if (limit <= 0) {
+        return 0; // Si el límite es menor o igual a 0, devuelve 0
+    }
+    return rand() % limit; // Genera un número aleatorio entre 0 y limit - 1
 }
 void snake_random_close(snake_game_t* p_game){
     
